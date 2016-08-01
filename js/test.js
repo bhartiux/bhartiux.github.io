ba1f48103cd0af1c1902de1d4ff022fbc065786a
bhartiux <bhartip0703@gmail.com>
2016-07-31 23:43:30 -0700

    some changes

--- js/test.js
+++ js/test.js
@@ -1,4 +1,36 @@
-function test() {
-	console.log("calling project-modal show");
-	$('#project-modal').modal('show');
-}
+
+$(document).ready(function(){
+	
+	//code to view image larger
+	
+	$('body').on('click', '.work-demo-img', function() {   
+		var url = $(this).attr('src');
+		$('#img-overlay #work-demo-large').attr('src', url).css('display', 'block').parent().fadeIn("slow");
+	});
+	
+	$('body').on('click', '.img-overlay-close', function(){
+		$(this).parent().fadeOut("slow");
+	})
+	
+	
+	//Check to see if the window is top if not then display button
+	$('#project-modal').scroll(function(){
+		if ($(this).scrollTop() > 200) {
+			$('.scrollToTop').fadeIn("slow");
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('body').on('click', '.scrollToTop', function(){ 
+		//if($('#project-modal').is(':hidden')){
+		//	$(window).scrollTop(0);
+		//} else { 
+			$('#project-modal').animate({scrollTop : 0}, 700);
+		//}
+		return false;
+	})
+	
+});
+   
